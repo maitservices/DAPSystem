e812fb6e54664d4afa8386ad8c84c627b115b072
Murilo Augusto de Oliveira <murilo.a.oliveirati@gmail.com>
2026-06-26 08:26:00 -0300

    Add route for Venda Simples page

--- app.js
+++ app.js
@@ -66,6 +66,11 @@ app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpPro
             controller: 'UsuariosCtrl',
             title: 'Sistema > Usuários'
         })
+        .when('/venda-simples', { 
+            templateUrl: 'views/venda-simples.html', 
+            controller: 'VendaSimplesCtrl',
+            title: 'Vendas > Vendas Simples'
+        })
         .otherwise({ redirectTo: '/inicio' });
 }]);
 
